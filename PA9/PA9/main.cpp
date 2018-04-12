841c51546943008f2cc862062dbe5eb32cbbd5ae
Kevin Evans <evans.kevinh@gmail.com>
2018-04-12 13:22:17 -0700

    made it 10% cooler

--- PA9/PA9/main.cpp
+++ PA9/PA9/main.cpp
@@ -1,16 +1,32 @@
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
+#include <cmath>
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-
+	sf::CircleShape shape(10);
+	shape.setFillColor(sf::Color::Red);
+	float i = 0;
 	while (window.isOpen())
 	{
 		sf::Event event;
+		sf::Color c(
+			(int)(cos(i * 10) * 100 + 155),
+			0,
+			0
+		);
+		shape.setFillColor(c);
+		shape.setPosition(
+			cos(i) * 100 + 100,
+			sin(i) * 100 + 100
+		);
+		i += 0.001;
+
 		while (window.pollEvent(event))
 		{
+			if (event.type == sf::Event::TextEntered) {
+				std::cout << (char)event.text.unicode << std::endl;
+			}
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
