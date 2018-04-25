7b5a4a24a30fc3d702feebc53eea7d0e85955dd7
Kevin Evans <evans.kevinh@gmail.com>
2018-04-24 20:08:52 -0700

    use pointers

--- PA9/PA9/GameScreen.h
+++ PA9/PA9/GameScreen.h
@@ -28,20 +28,20 @@ public:
 		Character player({ 100.0f, 100.0f });
 
 		std::list<Projectile> projectiles;
-		std::list<Zombie> zombies;
+		std::list<Character *> zombies;
 
 		for (int i = 0; i < 25; ++i) {
-			zombies.push_front(Zombie());
+			zombies.push_front(new Zombie());
 		}
 
-		
+
 		Background *testBackground = new Background(0, 0, 800, 600, "overcast.jpg");
-		Obstacle *testObstacle = new Obstacle(50, 250, 140, 100, "barricade1.png",100);
+		Obstacle *testObstacle = new Obstacle(50, 250, 140, 100, "barricade1.png", 100);
 		int boundscount = 0;
 
 		//Timepoint for delta time measurement
 		auto timePoint = std::chrono::steady_clock::now();
-		
+
 		while (window.isOpen()) {
 			// Process events
 			sf::Event event;
@@ -71,7 +71,7 @@ public:
 			sf::Vector2f dir = { 0.0f, 0.0f };
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 			{
-				if(!testObstacle->playerOnBottomBound(player)){
+				if (!testObstacle->playerOnBottomBound(player)) {
 					dir.y -= 1.0f;
 				}
 			}
@@ -117,31 +117,31 @@ public:
 			//enemy->update(dt);
 
 			/*if (testObstacle->playerWithinBounds(player)) {
-				std::cout << "player within barrier" <<boundscount<< std::endl;
-				boundscount++;
-				std::cout << "O:" << testObstacle->getBounds().getPosition().x << "," << testObstacle->getBounds().getPosition().y << std::endl
-					<< "P:" << player.getPosition().x << "," << player.getPosition().y << std::endl;
+			std::cout << "player within barrier" <<boundscount<< std::endl;
+			boundscount++;
+			std::cout << "O:" << testObstacle->getBounds().getPosition().x << "," << testObstacle->getBounds().getPosition().y << std::endl
+			<< "P:" << player.getPosition().x << "," << player.getPosition().y << std::endl;
 			}*/
-			for (Zombie &zombie : zombies)
+			for (Character *c : zombies)
 			{
-				direction = (player.getPosition() - zombie.getPosition());
+				direction = (player.getPosition() - c->getPosition());
 
-				zombie.setDirection(direction);
+				c->setDirection(direction);
 
-				if (testObstacle->playerOnTopBound(zombie) && direction.y > 0) {
-					zombie.update(0);
+				if (testObstacle->playerOnTopBound(*c) && direction.y > 0) {
+					c->update(0);
 				}
-				else if (testObstacle->playerOnLeftBound(zombie) && direction.x > 0) {
-					zombie.update(0);
+				else if (testObstacle->playerOnLeftBound(*c) && direction.x > 0) {
+					c->update(0);
 				}
-				else if (testObstacle->playerOnBottomBound(zombie) && direction.y < 0) {
-					zombie.update(0);
+				else if (testObstacle->playerOnBottomBound(*c) && direction.y < 0) {
+					c->update(0);
 				}
-				else if (testObstacle->playerOnRightBound(zombie) && direction.x < 0) {
-					zombie.update(0);
+				else if (testObstacle->playerOnRightBound(*c) && direction.x < 0) {
+					c->update(0);
 				}
 				else {
-					zombie.update(dt);
+					c->update(dt);
 				}
 
 			}
@@ -152,8 +152,8 @@ public:
 			testBackground->draw(window);
 			testObstacle->draw(window);
 			// Draw the sprite
-			for (Zombie &zombie : zombies) {
-				zombie.draw(window);
+			for (Character *c : zombies) {
+				c->draw(window);
 			}
 			player.draw(window);
 			//enemy->draw(window);
